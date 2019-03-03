56bf154a5dafafcc9267f6af27bc7d0ffcc7e3d5
juneezee <engzerjun@gmail.com>
2019-03-03 23:32:37 +0000

    Added README, inital partial page setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Installation
+Clone the repository:
+
+`git clone https://github.com/Juneezee/com3504.git`
+
+Install the dependencies
+
+`npm install`
+
+Start the server
+
+`npm start`
--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
