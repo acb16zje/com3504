abef73992be7480f6b6e449764909b40fb578f9d
juneezee <engzerjun@gmail.com>
2019-03-06 19:43:28 +0000

    SSL certificate and bottom navbar like Instagram

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Musicbee - enjoy the sweetest moment of music',
+    path: req.path
+  });
 });
 
 module.exports = router;
